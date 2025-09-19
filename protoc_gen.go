5ce1682e2490d450a5d97d131384c3383b6ef04f
Ruslan <rdantsov@tula.co>
2025-09-19 16:33:13 +0300

    fix package name

--- protoc_gen.go
+++ protoc_gen.go
@@ -1,3 +1,3 @@
-package osmetricsServer
+package osmetrics
 
 //go:generate  protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/metrics/metrics.proto
